82adf1571d8513fc1bc5ec2cfffa4f2fc23d4eb3
jamesnolan2013 <jamesnolan2013@gmail.com>
2025-04-12 21:44:49 +0100

    Moved index

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ let cachedData = null;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.get('/api/data', async (req, res) => {
   if (cachedData) {
